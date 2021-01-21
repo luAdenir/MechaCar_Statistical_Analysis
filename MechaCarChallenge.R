5cb79ab532006965a7cbe510afca4900c22028ba
luAdenir <71112826+luAdenir@users.noreply.github.com>
2021-01-20 21:11:03 -0800

    MechaCar Challenge RScript
    linear regression and t.test

---
+++ MechaCarChallenge.R
@@ -0,0 +1,22 @@
+library(dplyr)
+library(tidyverse)
+
+#Deliverable_1
+MechaCar_table<-read.csv(file="MechaCar_mpg.csv", check.names = F, stringsAsFactors =F)
+lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=MechaCar_table)
+summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=MechaCar_table))
+
+#Deliverable_2
+SuspensionCoil_table<-read.csv(file="Suspension_Coil.csv", check.names = F, stringsAsFactors =F)
+
+total_summary<-SuspensionCoil_table%>%summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+lot_summary<-SuspensionCoil_table%>%group_by(Manufacturing_Lot)%>%summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+#Deliverable_3
+t.test(SuspensionCoil_table$PSI,mu=1500)
+
+#T.test for Manufacturing Lot Subset
+t.test(subset(SuspensionCoil_table, Manufacturing_Lot=="Lot1")$PSI,mu=15000)
+t.test(subset(SuspensionCoil_table, Manufacturing_Lot=="Lot2")$PSI,mu=15000)
+t.test(subset(SuspensionCoil_table, Manufacturing_Lot=="Lot3")$PSI,mu=15000)
